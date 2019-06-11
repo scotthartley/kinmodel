f4c3fe309bfa1cef5caa17bbaed9dbe85a42f589
Scott Hartley <scott.hartley@miamioh.edu>
2019-06-11 14:50:30 -0400

    Add MA_shared_int_E_hyd_ss_AddE model; add eqns to MA_shared_int_E_hyd_ss models

--- kinmodel/models/MA_shared_int_E_hyd_ss.py
+++ kinmodel/models/MA_shared_int_E_hyd_ss.py
@@ -4,7 +4,7 @@ from ..KineticModel import KineticModel
 
 def equations(concs, t, *ks):
     Ac, E, U, An = concs
-    k1, k_2, k4, K = ks
+    k1, k_2, K, k4 = ks
 
     return [(- k1*Ac*E - (k1*Ac**2*E)/(Ac+K) - (k_2*An*Ac)/(Ac+K) + k_2*An
              + (k1*K*Ac*E)/(Ac+K) + (k_2*K*An)/(Ac+K)),
@@ -25,14 +25,14 @@ model = KineticModel(
                  E ---> U      (k4)
 
         Steady-state approximation with K=k3/k2.
-        Orders: k1, k_2, k4, K; Ac, E, U, An.\
+        Orders: k1, k_2, K, k4; Ac, E, U, An.\
         """),
     kin_sys=equations,
-    ks_guesses=[0.02, 0.03, 0.1, 10],
+    ks_guesses=[0.02, 0.03, 10, 0.1],
     ks_constant=[],
     conc0_guesses=[50, 50],
     conc0_constant=[0, 0],
-    k_var_names=["k1", "k-2", "k4", "K"],
+    k_var_names=["k1", "k-2", "K", "k4"],
     k_const_names=[],
     conc0_var_names=["[Acid]0", "[EDC]0"],
     conc0_const_names=["[U]0", "[An]0"],
@@ -45,21 +45,37 @@ model = KineticModel(
             lambda cs, ks: (ks[0]*cs[0]**2*cs[1])/(cs[0]+ks[2]),
             lambda cs, ks: (ks[1]*cs[3]*cs[0])/(cs[0]+ks[2]),
             lambda cs, ks: ks[0]*cs[1]*cs[0],
-            lambda cs, ks: ks[3]*cs[1], ],
+            lambda cs, ks: ks[3]*cs[1],
+            lambda cs, ks: (ks[0]*ks[2]*cs[0]*cs[1])/(cs[0]+ks[2]),
+            lambda cs, ks: (ks[1]*ks[2]*cs[3])/(cs[0]+ks[2]),
+            ],
     int_eqn_desc=[
             "k_2*An",
             "(k1*Ac^2*E)/(Ac+K)",
             "(k_2*An*Ac)/(Ac+K)",
             "k1*E*Ac",
-            "k4*E", ],
+            "k4*E",
+            "(k1*K*Ac*E)/(Ac+K)",
+            "(k_2*K*An)/(Ac+K)",
+            ],
     calcs=[
             lambda cs, ks, ints: max(cs[:, 3]),
             lambda cs, ks, ints: cs[:, 3][-1],
-            lambda cs, ks, ints: ints[1][1]/cs[:, 1][0], ],
+            lambda cs, ks, ints: ints[1][1]/cs[:, 1][0],
+            lambda cs, ks, ints: (ints[0][1] - ints[2][1])/cs[:, 1][0],
+            lambda cs, ks, ints: (ints[3][1] - ints[5][1])/cs[:, 1][0],
+            lambda cs, ks, ints: (ints[4][1] + ints[5][1])/cs[:, 1][0],
+            lambda cs, ks, ints: ints[2][1]/ints[6][1],
+            ],
     calcs_desc=[
             "Maximum An",
             "Final An",
-            "An yield from (∫k1*Ac^2*E)/(Ac+K)dt)/E0"],
+            "Yield (∫(k1*Ac^2*E)/(Ac+K)dt)/E0",
+            "Yield (∫(k_2An)dt - ∫(k_2*An*Ac)/(Ac+K)dt)/E0",
+            "Yield (∫(k1*E*Ac)dt - ∫(k1*K*Ac*E)/(Ac+K)dt)/E0",
+            "Wasted E (∫(k4*E)dt + ∫(k1*K*Ac*E)/(Ac+K)dt)/E0",
+            "Net exchange ∫(k_2*An*Ac)/(Ac+K)dt / ∫((k_2*K*An)/(Ac+K))dt",
+            ],
     lifetime_conc=[3],
     rectime_conc=[0],
     )

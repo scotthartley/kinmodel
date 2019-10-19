0fe7b7718808a7d8672aca4426ad83fe4b6f14b7
Scott Hartley <scott.hartley@miamioh.edu>
2019-10-19 11:30:23 -0400

    Add DA_explicit_DA2_two_int_ss and DA_explict_DA3_two_int_ss models

---
+++ kinmodel/models/DA_explicit_DA2_two_int_ss_ind.py
@@ -0,0 +1,28 @@
+import textwrap
+import numpy as np
+from ..KineticModel import IndirectKineticModel
+
+
+model = IndirectKineticModel(
+    name="DA_explicit_DA2_two_int_ss_ind",
+    parent_model_name="DA_explicit_DA2_two_int_ss",
+    description=textwrap.dedent("""\
+        Indirect version of the DA_explicit_two_int_DA2_ss model, using
+        total diacid and total anhydride concentration.\
+        """),
+    conc_mapping=lambda c: np.array([c[:, 0]+c[:, 3],
+                                     c[:, 1],
+                                     c[:, 2],
+                                     c[:, 3],
+                                     c[:, 4]]).transpose(),
+    legend_names=["Diacid", "EDC", "Urea", "Linear", "Cyclic"],
+    top_plot=[1, 2],
+    bottom_plot=[0, 3, 4],
+    sort_order=[2, 3, 4, 0, 1],
+    int_eqn=[
+        ],
+    int_eqn_desc=[
+        ],
+    lifetime_conc=[],
+    rectime_conc=[],
+    )
---
+++ kinmodel/models/DA_explicit_DA3_two_int_ss.py
@@ -0,0 +1,80 @@
+import textwrap
+from ..KineticModel import KineticModel
+
+
+def equations(concs, t, *ks):
+    DA1, E, U, DA2, DA3, C = concs
+    k1, K1, EM1, K2, EM2, k2C, k2L = ks
+
+    # Return the equations for concs
+    return [
+        (2*DA3*k2L + k2L*DA2 - k1*DA1*E
+            - (DA1*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2)
+            + (K2*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2)
+            - (k1*DA1**2*E)/(EM1 + K1 + DA1 + DA2)
+            - (2*DA3*k2L*DA1)/(K2 + DA1) + (K1*k1*DA1*E)/(EM1 + K1 + DA1 + DA2)
+            - (k1*DA1*DA2*E)/(K1 + DA1)),
+        - k1*DA1*E - k1*DA2*E,
+        + k1*DA1*E + k1*DA2*E,
+        (2*DA3*k2L - k2L*DA2 - k1*DA2*E
+            + (DA1*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2)
+            - (DA2*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2)
+            + (2*DA3*K2*k2L)/(K2 + DA1) + (k1*DA1**2*E)/(EM1 + K1 + DA1 + DA2)
+            + (K1*k1*DA2*E)/(K1 + DA1)
+            - (k1*DA1*DA2*E)/(EM1 + K1 + DA1 + DA2)),
+        ((DA2*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2) - 4*DA3*k2L
+            + (2*DA3*k2L*DA1)/(K2 + DA1)
+            + (k1*DA1*DA2*E)/(EM1 + K1 + DA1 + DA2)
+            + (k1*DA1*DA2*E)/(K1 + DA1)),
+        ((EM2*(2*DA3*k2L + k2C*C + k2L*DA2))/(EM2 + K2 + DA1 + DA2) - k2C*C
+            + (EM1*k1*DA1*E)/(EM1 + K1 + DA1 + DA2)),
+    ]
+
+
+model = KineticModel(
+    name="DA_explicit_DA3_two_int_ss",
+    description=textwrap.dedent("""\
+        Simple model for diacid assembly with explicit consideration of
+        linear anhydride intermediates, capped at the trimer (DA3).
+        Separate intermediates for EDC consumption and anhydride
+        exchange are used.
+
+             DA1 + E ---> I1         (k1)
+                  I1 ---> DA1 + U    (kih)
+                  I1 ---> C + U      (kiC)
+            I1 + DA1 ---> DA2        (kiL)
+            I1 + DA2 ---> DA3        (kiL)
+                 DA2 <--> DA1 + Ip1  (k2L, km2L)
+                   C <--> Ip1        (k2C, km2C)
+                 DA3 <--> DA2 Ip1    (2*k2L, km2L)
+                 DA3 <-->  Ip2 + DA1 (2*k2L, km2L)
+                 Ip1 ---> DA1        (k3)
+                 Ip2 ---> DA2        (k3)
+             DA2 + E ---> I2         (k1)
+                  I2 ---> DA2 + U    (kih)
+            I2 + DA1 ---> DA3        (kiL)
+
+        Steady-state approximations with K1 = kih/kiL, EM1 = kiC/kiL,
+        K2 = k3/km2L, and EM2 = km2C/km2L.
+        Orders: k1, K1, EM1, K2, EM2, k2C, k2L; DA1, E, U, DA2, DA3, C.\
+        """),
+    kin_sys=equations,
+    ks_guesses=[0.01, 25, 10, 0.01, 10, 100, 100],
+    ks_constant=[],
+    conc0_guesses=[25, 50],
+    conc0_constant=[0, 0, 0, 0],
+    k_var_names=["k1", "K1", "EM1", "K2", "EM2", "k2C", "k2L"],
+    k_const_names=[],
+    conc0_var_names=["[DA1]0", "[EDC]0"],
+    conc0_const_names=["[U]0", "[DA2]0", "[DA3]0", "[C]0"],
+    legend_names=["DA1", "EDC", "Urea", "DA2", "DA3", "Cy"],
+    top_plot=[1, 2],
+    bottom_plot=[0, 3, 4, 5],
+    sort_order=[],
+    int_eqn=[
+        ],
+    int_eqn_desc=[
+        ],
+    lifetime_conc=[3, 4, 5],
+    rectime_conc=[0],
+    )

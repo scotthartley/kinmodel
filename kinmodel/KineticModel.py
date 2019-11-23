51f8d5c3b938e6b6aa9e55d8db41f83bbc354924
Scott Hartley <scott.hartley@miamioh.edu>
2019-11-23 12:05:08 -0500

    Tweak cc generation such that optimized params are always included.

--- kinmodel/KineticModel.py
+++ kinmodel/KineticModel.py
@@ -490,20 +490,18 @@ class KineticModel:
         results = []
         for p1_ind in range(total_num_params-1):
             for p2_ind in range(p1_ind+1, total_num_params):
-                p1_low, p1_high = self._bracket_param(
+                p1_vals = self._bracket_param(
                         reg_info['all_params'][p1_ind],
                         all_params_bot[p1_ind],
                         all_params_top[p1_ind],
+                        num_intervals,
                         cc_mult=cc_mult)
-                p2_low, p2_high = self._bracket_param(
+                p2_vals = self._bracket_param(
                         reg_info['all_params'][p2_ind],
                         all_params_bot[p2_ind],
                         all_params_top[p2_ind],
+                        num_intervals,
                         cc_mult=cc_mult)
-                p1_vals = self._divide_into_intervals(
-                        p1_high, p1_low, num_intervals)
-                p2_vals = self._divide_into_intervals(
-                        p2_high, p2_low, num_intervals)
 
                 var_params = list(reg_info['all_params'])
                 var_params_ind = [x for x in range(len(
@@ -801,14 +799,7 @@ class KineticModel:
             print(", ".join(a for a in models), "are currently available.")
             sys.exit(1)
 
-    @staticmethod
-    def _divide_into_intervals(top, bottom, num):
-        """Given top and bottom, divides into num even intervals.
-        """
-        delta = (top - bottom)/(num - 1)
-        return [bottom + i*delta for i in range(num)]
-
-    def _bracket_param(self, param, low, high, cc_mult=2):
+    def _bracket_param(self, param, low, high, num_iterations, cc_mult=2):
         """Used to generated confidence contours. Returns the upper and
         lower limits that should be used for a given value of param and
         high and low CIs.
@@ -817,8 +808,51 @@ class KineticModel:
         delta_low = param - low
         delta = cc_mult*max(delta_high, delta_low)
 
-        return (max(self.bounds[0], param - delta),
-                min(self.bounds[1], param + delta))
+        # We want the CCs to always include the actual optimized param,
+        # ideally in the center but offset if the parameter would be
+        # outside of the bounds. If an even number of iterations is
+        # specified, such that there is no exact center of the plot, the
+        # range favors the high end.
+
+        if ((param + delta) <= self.bounds[1] and
+                (param - delta) >= self.bounds[0]):
+            if num_iterations % 2 == 1:
+                interval = 2*delta/(num_iterations - 1)
+                bottom = param - (num_iterations-1)/2 * interval
+            else:
+                interval = 2*delta/num_iterations
+                bottom = param - (num_iterations/2 - 1) * interval
+        elif ((self.bounds[0] + 2*delta) <= self.bounds[1] and
+                (param - delta) < self.bounds[0]):
+            # Bounded on low end.
+            max_interval = 2*delta/(num_iterations - 1)
+            # Integer number of intervals to the actual param value.
+            n_to_p = math.ceil((param - self.bounds[0])/max_interval)
+            interval = (param - self.bounds[0])/n_to_p
+            bottom = self.bounds[0]
+        elif ((param + delta) > self.bounds[1] and
+                (self.bounds[1] - 2*delta) >= self.bounds[0]):
+            # Bounded on high end.
+            max_interval = 2*delta/(num_iterations - 1)
+            n_to_p = math.ceil((self.bounds[1] - param)/max_interval)
+            interval = (self.bounds[1] - param)/n_to_p
+            bottom = self.bounds[1] - (num_iterations-1)*interval
+        else:
+            # Bounded on both sides.
+            max_interval = (self.bounds[1] - self.bounds[0])/(num_iterations - 1)
+            n_to_p_low = (param - self.bounds[0])/max_interval
+            n_to_p_high = (self.bounds[1] - param)/max_interval
+            if (n_to_p_low % 1) < (n_to_p_high % 1):
+                n_to_p = math.ceil(n_to_p_low)
+            else:
+                n_to_p = math.floor(n_to_p_low)
+            interval = (param - self.bounds[0])/n_to_p
+            bottom = param - n_to_p*interval
+
+        assert ((bottom >= self.bounds[0])
+                and (bottom + interval*(num_iterations - 1)))
+
+        return [bottom + i*interval for i in range(num_iterations)]
 
 
 class IndirectKineticModel(KineticModel):
